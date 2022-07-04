988ad69e633240f3e200792ae44c0b6280bae671
Xavian Moody <XavianMoody1025@gmail.com>
2022-07-04 03:52:58 -0400

    Continue css

---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// This will fetch data of location the user is searching
+// fetch(
+//   `http://api.openweathermap.org/geo/1.0/direct?q=${"Lexington-Fayette"}&limit=5&appid=97773d5af5401e1b00aed3a63620b57e`
+// )
+//   .then((data) => data.json())
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch(() => {
+//     console.log("Failed");
+//   });
+
+// This will fecth the weather data. Must pass location latitude and longitude
+// const test = (lat, lon) => {
+//   fetch(
+//     `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=97773d5af5401e1b00aed3a63620b57e`
+//   )
+//     .then((data) => data.json())
+//     .then((data) => {
+//       console.log(data);
+//     })
+//     .catch((failed) => {
+//       console.log("Failed");
+//     });
+// };
