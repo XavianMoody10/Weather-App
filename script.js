980d1ed55126744443128c5fab8a8d76b6a33893
Xavian Moody <XavianMoody1025@gmail.com>
2022-07-06 23:58:05 -0400

    Final Commit

--- script.js
+++ script.js
@@ -1,12 +1,58 @@
 "use strict";
-const searchBar = document.querySelector("#search input");
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// REUSABLE FUNCTIONS
+
+// THIS FUNCTION REFRESHES THESUGGESTION BOX
+const refreshSuggestion = (data) => {
+  const suggestionBox = document.querySelector("#suggestion-box");
+
+  // Remove old options in the suggestion box
+  [...suggestionBox.children].forEach((child) => {
+    suggestionBox.removeChild(child);
+  });
+
+  // Appends new options into suggestion box
+  data.forEach((options) => {
+    const paragraph = document.createElement("p");
+    paragraph.classList.add("options");
+    paragraph.textContent = `${options.name}, ${options.state}`;
+    suggestionBox.append(paragraph);
+  });
+};
+
+// IF A ERROR ACCURES DURING A FETCH API CALL, THIS FUNCTION WILL EXECUTE
+const failedError = () => {
+  const city = document.querySelector("#city");
+  const country = document.querySelector("#country");
+  const suggestionBox = document.querySelector("#suggestion-box");
+  const searchBar = document.querySelector("#search input");
+
+  if (searchBar.value.length >= 0) {
+    city.textContent = "No Results";
+    country.textContent = "";
+  }
+
+  [...suggestionBox.children].forEach((child) => {
+    suggestionBox.removeChild(child);
+  });
+};
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // WHEN THE PAGE LOADS, THE USER'S CURRENT LOCATION'S WEATHER WILL DISPLAY
 function getUserCoords() {
+  const suggestionBox = document.querySelector("#suggestion-box");
+
   navigator.geolocation.getCurrentPosition((position) => {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
     getWeather(latitude, longitude);
+    console.log("LOADING...");
+
+    setTimeout(() => {
+      // ADD THE STYLE CLASS TO THE FIRST ELEMENT IN THE SUGGESTION BOX
+      suggestionBox.children[0].classList.add("option-select");
+    }, 300);
   });
 }
 
@@ -17,16 +63,23 @@ const getWeather = (lat, lon) => {
   )
     .then((data) => data.json())
     .then((data) => {
+      console.log(data);
       displayWeather(data);
       locationSearch(data.name);
     })
-    .catch((failed) => {
+    .catch(() => {
       console.log("Failed");
+      failedError();
     });
 };
 
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 // This will fetch data of location the user is searching. THIS WILL DSIPLAY THE CITY, STATE, AND COUNTRY
 const locationSearch = (location) => {
+  const suggestionBox = document.querySelector("#suggestion-box");
+  const searchBar = document.querySelector("#search input");
+
   fetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=97773d5af5401e1b00aed3a63620b57e`
   )
@@ -34,9 +87,11 @@ const locationSearch = (location) => {
     .then((data) => {
       console.log(data);
       getLocation(data);
+      // suggestionBox.children[0].classList.add("option-select");
     })
     .catch(() => {
       console.log("Failed");
+      failedError();
     });
 };
 
@@ -44,6 +99,8 @@ const locationSearch = (location) => {
 
 // GET LOCATION DATA
 const getLocation = (data) => {
+  const searchBar = document.querySelector("#search input");
+
   // If searchbar contains a value.
   if (
     (searchBar.value.length == 0 && data.length > 0) ||
@@ -78,10 +135,13 @@ const locationDOM = (data) => {
     cityName: data.name,
     stateName: data.state,
     countryName: data.country,
+    dataDOM: function () {
+      cityDOM.textContent = `${this.cityName}, ${this.stateName}`;
+      countryDOM.textContent = `${this.countryName}`;
+    },
   };
 
-  cityDOM.textContent = `${locationDOMData.cityName}, ${locationDOMData.stateName}`;
-  countryDOM.textContent = `${locationDOMData.countryName}`;
+  locationDOMData.dataDOM();
 };
 
 //// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -90,18 +150,7 @@ const locationDOM = (data) => {
 const displaySuggestions = (data) => {
   const suggestionBox = document.querySelector("#suggestion-box");
 
-  // Remove old options in the suggestion box
-  [...suggestionBox.children].forEach((child) => {
-    suggestionBox.removeChild(child);
-  });
-
-  // Appends new options into suggestion box
-  data.forEach((options) => {
-    const paragraph = document.createElement("p");
-    paragraph.classList.add("options");
-    paragraph.textContent = `${options.name}, ${options.state}`;
-    suggestionBox.append(paragraph);
-  });
+  refreshSuggestion(data);
 
   // When the user clicks on any of the options
   document.querySelectorAll("*").forEach((el) => {
@@ -110,17 +159,24 @@ const displaySuggestions = (data) => {
 
       if (e.target.classList.contains("options")) {
         const index = [...suggestionBox.children].indexOf(e.target);
+        const lat = data[index].lat;
+        const lon = data[index].lon;
+
         displayLocation(data[index], data);
 
+        suggestionBox.children[index].classList.toggle("option-select");
+
         fetch(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${data[index].lat}&lon=${data[index].lon}&appid=97773d5af5401e1b00aed3a63620b57e`
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=97773d5af5401e1b00aed3a63620b57e`
         )
           .then((data) => data.json())
           .then((data) => {
+            console.log(data);
             displayWeather(data);
           })
-          .catch((failed) => {
+          .catch(() => {
             console.log("Failed");
+            failedError();
           });
       }
     });
@@ -137,25 +193,86 @@ const displayWeather = (data) => {
   const weatherImage = document.querySelector("#visual img");
   const weatherDescription = document.querySelector("#visual p");
 
-  degrees.textContent = `${data.main.temp} \u00B0F`;
-  humidity.textContent = `${data.main.humidity}%`;
-  wind.textContent = `${data.wind.speed} mph`;
-  weatherImage.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-  weatherDescription.textContent = data.weather[0].description.split(",");
+  const weatherData = {
+    degrees: data.main.temp,
+    humidity: data.main.humidity,
+    wind: data.wind.speed,
+    weatherImage: data.weather[0].icon,
+
+    weatherDescription: function () {
+      const wd = data.weather[0].description.split(" ");
+
+      const stringMap = wd.map((word) => {
+        const cap = word[0].toUpperCase();
+        const rest = word.substring(1, word.length);
+        const combine = cap.concat(rest);
+        return combine;
+      });
+
+      weatherDescription.textContent = stringMap.join(" ");
+    },
+
+    weatherDOM: function () {
+      degrees.textContent = `${this.degrees} \u00B0F`;
+      humidity.textContent = `${this.humidity}%`;
+      wind.textContent = `${this.wind} mph`;
+      weatherImage.src = `http://openweathermap.org/img/wn/${this.weatherImage}@2x.png`;
+    },
+  };
+
+  weatherData.weatherDescription();
+  weatherData.weatherDOM();
 };
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// THIS FUNCTION DISPLAYS THE DATE AND TIME OF EACH SEARCH
+const searchTimestamp = () => {
+  const monthDOM = document.querySelector("#month");
+  const dayDOM = document.querySelector("#day");
+  const yearDOM = document.querySelector("#year");
+  const date = new Date();
 
-searchBar.addEventListener("input", (e) => {
-  const city = document.querySelector("#city");
-  const country = document.querySelector("#country");
+  const allmonths = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-  if (e.target.value == "") {
-    city.textContent = "No Results";
-    country.textContent = "";
-  }
+  const dateData = {
+    month: date.getMonth(),
+    day: date.getDate(),
+    year: date.getFullYear(),
 
-  locationSearch(e.target.value);
-});
+    dateDOM: function () {
+      monthDOM.textContent = allmonths[this.month].toUpperCase();
+      dayDOM.textContent = this.day;
+      yearDOM.textContent = this.year;
+    },
+  };
 
-window.addEventListener("load", getUserCoords);
+  dateData.dateDOM();
+};
+searchTimestamp();
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// THIS FUNCTION CONTAINS ALL EVENT HANDLERS WITHIN THE PROGRAM
+const events = () => {
+  const searchBar = document.querySelector("#search input");
+
+  searchBar.addEventListener("input", (e) => {
+    locationSearch(e.target.value);
+  });
+
+  window.addEventListener("load", getUserCoords);
+};
+
+events();
